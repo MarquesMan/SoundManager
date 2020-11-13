b9d058d956f5d4d49593fdabe1bd60413abec8ea
MarquesMan <marquesnavarezi@hotmail.com>
2020-11-13 14:57:38 -0400

    Initial commit new file:   .gitignore new file:   Assets/Animations.meta new file:   Assets/Animations/Idle (32x32)_0.controller new file:   Assets/Animations/Idle (32x32)_0.controller.meta new file:   Assets/Animations/Idle.anim new file:   Assets/Animations/Idle.anim.meta new file:   Assets/Animations/On (16x32)_0 1.controller new file:   Assets/Animations/On (16x32)_0 1.controller.meta new file:   Assets/Animations/On (16x32)_0.controller new file:   Assets/Animations/On (16x32)_0.controller.meta new file:   Assets/Animations/Run (32x32)_0.controller

---
+++ Assets/Scripts/SimpleMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleMovement : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+    private float inputForce;
+    private Vector2 speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Vector2.zero;
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        inputForce = Input.GetAxisRaw("Horizontal");
+        if (inputForce != 0)
+            spriteRenderer.flipX = inputForce < 0;
+
+        animator.SetBool("run", inputForce != 0);
+        speed.x = inputForce * 5;
+        rigidBody.velocity =  speed;
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# SoundManager
+
+O que temos implementado:
+
+  - Um Gerenciador de áudio singleton
+  - Audio spacial para jogos 2D
+  - Outra coisa aqui
